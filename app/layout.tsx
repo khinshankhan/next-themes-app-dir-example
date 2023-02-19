699b66b30c0fce2170d7d676dd13f5dfd5cd855e
Khinshan Khan <shan@uptogood.dev>
2023-02-19 13:07:46 -0500

    add next themes to app dir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-import './globals.css'
+import "./globals.css";
+import { ThemeProvider } from "./theme-provider";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
